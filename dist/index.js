576493b486dce6295450fb92e0af86f335b6fdb1
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-12-29 20:30:48 +0500

    Add Middleware & Error Handling

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,68 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const note_1 = __importDefault(require("./models/note"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const express_1 = __importDefault(require("express"));
+const http_errors_1 = __importStar(require("http-errors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.send("Check");
-});
 const PORT = process.env.PORT;
+app.use(express_1.default.json());
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const notes = yield note_1.default.find().exec();
+    res.status(200).json(notes);
+}));
+app.use((req, res, next) => {
+    next((0, http_errors_1.default)(404, "Endpoint Not Found"));
+});
+app.use((error, req, res, next) => {
+    console.error(error);
+    let errorMessage = "An Unknown Error Occurred";
+    let statusCode = 500;
+    if ((0, http_errors_1.isHttpError)(error)) {
+        statusCode = error.status;
+        errorMessage = error.message;
+    }
+    res.status(statusCode).json({ error: errorMessage });
+});
 mongoose_1.default.connect(process.env.DATABASE).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server Running On PORT: ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Server Running On PORT: ${PORT}`));
 })
     .catch(console.error);
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,58 @@
+import NoteModel from "./models/note";
 import mongoose from "mongoose";
-import express from "express";
+import express, {
+    NextFunction,
+    Request,
+    Response
+} from "express";
+import createHttpError, { isHttpError } from "http-errors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Check");
-});
-
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+
+    const notes = await NoteModel.find().exec();
+
+    res.status(200).json(notes);
+
+});
+
+app.use((req, res, next) => {
+
+    next(createHttpError(404, "Endpoint Not Found"));
+
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+
+    console.error(error);
+
+    let errorMessage = "An Unknown Error Occurred";
+
+    let statusCode = 500;
+
+    if (isHttpError(error)) {
+
+        statusCode = error.status;
+
+        errorMessage = error.message;
+    }
+
+    res.status(statusCode).json({ error: errorMessage });
+});
+
 mongoose.connect(process.env.DATABASE!).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server Running On PORT: ${PORT}`);
-    });
+
+    app.listen(PORT, () => console.log(`Server Running On PORT: ${PORT}`));
+
 })
     .catch(console.error);
+
+export default app;
