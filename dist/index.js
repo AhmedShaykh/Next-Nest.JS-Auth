7ea9ca4b678719c4690ba0c80344a3c66160db81
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-12-31 14:57:42 +0500

    Add Cors

--- dist/index.js
+++ dist/index.js
@@ -32,11 +32,13 @@ const express_1 = __importDefault(require("express"));
 const http_errors_1 = __importStar(require("http-errors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const morgan_1 = __importDefault(require("morgan"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 app.use(express_1.default.json());
 app.use((0, morgan_1.default)("dev"));
+app.use((0, cors_1.default)());
 app.use("/api/notes", notes_1.default);
 app.use((req, res, next) => {
     next((0, http_errors_1.default)(404, "Endpoint Not Found"));
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import express, {
 import createHttpError, { isHttpError } from "http-errors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import cors from "cors";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use(morgan("dev"));
+
+app.use(cors());
 
 app.use("/api/notes", notesRoutes);
 
@@ -43,7 +46,6 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     }
 
     res.status(statusCode).json({ error: errorMessage });
-
 });
 
 mongoose.connect(process.env.DATABASE!).then(() => {
